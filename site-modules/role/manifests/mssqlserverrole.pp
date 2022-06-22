c4b6af61dba78ab622332f55faadc33daf8d49c6
shanebsutton <78883537+shanebsutton@users.noreply.github.com>
2022-06-22 16:12:30 -0500

    Remove chocolatey class

--- site-modules/role/manifests/mssqlserverrole.pp
+++ site-modules/role/manifests/mssqlserverrole.pp
@@ -5,10 +5,6 @@ class role::mssqlserverrole {
   include profile::base
   include chocolatey
   
-  class { 'chocolatey':
-    log_output => true,
-  }
-  
   package { 'notepadplusplus':
     ensure   => '8.4.2',
     provider => 'chocolatey',
