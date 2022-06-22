95b8d1912131b895b8e80afb61ef63301439132c
shanebsutton <78883537+shanebsutton@users.noreply.github.com>
2022-06-22 15:32:07 -0500

    Add chocolatey

--- site-modules/role/manifests/mssqlserverrole.pp
+++ site-modules/role/manifests/mssqlserverrole.pp
@@ -3,6 +3,7 @@ class role::mssqlserverrole {
   #This role would be made of all the profiles that need to be included to make a database server work
   #All roles should include the base profile
   include profile::base
+  include chocolatey
   
   package { 'notepadplusplus':
     ensure   => '8.4.2',
