6786a0cb88dc43eb7f26a0421423c30074c2f61d
shanebsutton <78883537+shanebsutton@users.noreply.github.com>
2022-06-29 16:36:16 -0500

    Update mssqlserverrole.pp

--- site-modules/role/manifests/mssqlserverrole.pp
+++ site-modules/role/manifests/mssqlserverrole.pp
@@ -32,7 +32,7 @@ class role::mssqlserverrole {
   package { 'InstallSSMS':
     ensure    => 'Present',
     name      => 'SSMS-Setup-ENU_1812',
-    path      => 'C:\\Temp\\SSMS-Setup-ENU_1812.exe',
+    source    => 'C:\\Temp\\SSMS-Setup-ENU_1812.exe',
     arguments => '/install /quiet /norestart',
     productid => '990516C3-F457-4E25-B13E-B1599B2F4156',
   }
